0a350816b0e132fcca49ae08d0feca0b9e35848c
hmmyalex@gmail.com <hmmyalex@gmail.com>
2020-04-17 21:18:59 +0300

    Faster Ex1_C++ Ex2_C++

---
+++ Series1/Ex1_C++/try_log2nfromGiannis.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cstring>
+
+#define BSIZE 1<<15
+// Fast input taken from Dimitris Fotakis
+char buffer[BSIZE];
+long bpos = 0L, bsize = 0L;
+long readLong(FILE* argv)
+{
+    long d = 0L, x = 0L; char c;
+    while(1) {
+        if (bpos >= bsize) {
+            bpos = 0;
+            if (feof(argv)) { // Edw trww seg
+                
+                return x;}
+            bsize = fread(buffer, 1, BSIZE, argv);
+        }
+        c = buffer[bpos++];
+        if (c >= '0' && c <= '9') { x = x*10 + (c-'0'); d = 1; }
+        else if (d == 1) return x;
+    }
+    return -1;
+}
+
+
+// Fast Log2 taken from StackOverflow url: https://stackoverflow.com/questions/11376288/fast-computing-of-log2-for-64-bit-integers
+const int tab64[64] = {
+    63,  0, 58,  1, 59, 47, 53,  2,
+    60, 39, 48, 27, 54, 33, 42,  3,
+    61, 51, 37, 40, 49, 18, 28, 20,
+    55, 30, 34, 11, 43, 14, 22,  4,
+    62, 57, 46, 52, 38, 26, 32, 41,
+    50, 36, 17, 19, 29, 10, 13, 21,
+    56, 45, 25, 31, 35, 16,  9, 12,
+    44, 24, 15,  8, 23,  7,  6,  5};
+
+int log2_64 (uint64_t value)
+{
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    value |= value >> 32;
+    return tab64[((uint64_t)((value - (value >> 1))*0x07EDD5E59A4E28C2)) >> 58];
+}
+
+int powerOf2[50];
+bool flag = 1;
+int max_delta = 0;
+
+bool recursiveFoo (long N, long K){
+    if (N==K){
+        powerOf2[0]+=K;
+        return 1;
+    }
+    if (K > N) return 0;
+    if (K == 0 && N != 0) return 0;
+    
+    if (K == 0) return 1;
+    else {
+        int delta = log2_64(N-K+1);
+        if (flag) {
+            max_delta = delta;
+            flag = 0;
+        }
+        powerOf2[delta]++;
+        // Fastest Power of 2 I found it by shifting!!!
+        return recursiveFoo(N - (1 << delta), K - 1);
+    }
+
+}
+
+int main (int argc, char** argv){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    FILE * pFile;
+    pFile = fopen (argv[1], "r");
+    int T = readLong(pFile);
+    for (int i = 0; i < T; i++){
+        // Initializations
+        //Fast Initialization of the array
+        memset(powerOf2,0,sizeof(powerOf2));
+        flag = 1;
+        max_delta = 0;
+        
+        long N = readLong(pFile);
+        long K = readLong(pFile);
+        bool douleuei = recursiveFoo(N,K);
+        if(!douleuei) printf("[]\n");
+        else {
+            printf("[");
+            for (int i = 0; i < max_delta; i++){
+                printf("%d,",powerOf2[i]);
+            }
+            printf ("%d]\n",powerOf2[max_delta]);
+        }
+        
+    }
+}
---
+++ Series1/Ex2_C++/coronoFast.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+#define BSIZE 1<<15
+
+// Fast input taken from Dimitris Fotakis
+char buffer[BSIZE];
+long bpos = 0L, bsize = 0L;
+long readLong(FILE* argv)
+{
+    long d = 0L, x = 0L; char c;
+    while(1) {
+        if (bpos >= bsize) {
+            bpos = 0;
+            if (feof(argv)) return x;
+            bsize = fread(buffer, 1, BSIZE, argv);
+        }
+        c = buffer[bpos++];
+        if (c >= '0' && c <= '9') { x = x*10 + (c-'0'); d = 1; }
+        else if (d == 1) return x;
+    }
+    return -1;
+}
+
+// Global Variables
+std::vector <unsigned int> Node[1000000];
+std::queue <unsigned int> singleEdgedNodes;
+unsigned int degree[1000000];
+unsigned int numOfChildren[1000000];
+std::vector<unsigned int> finalRoots;
+unsigned int N = 0;
+unsigned int father = 0; // to use as last element
+unsigned int numVisited = 0; // to check if all visited // edge-case graph is tree
+unsigned int numFinalRoots = 0; // for output
+
+// Track children and find cycle
+bool recursiveFoo(){
+    // Find all nodes with only one edge
+    for (unsigned int i = 0; i < N; i++) {
+        if (degree[i] == 0) return 0; 
+        else if (degree[i] == 1) singleEdgedNodes.push(i);
+    }
+    // Find num of children to all nodes
+    while(!singleEdgedNodes.empty()){
+        // Pop Front Leaf
+        unsigned int leaf = singleEdgedNodes.front();
+        singleEdgedNodes.pop();
+        //
+        father = Node[leaf][0];
+        // Erase fathers edge to leaf
+        Node[father].erase(std::remove(Node[father].begin(), Node[father].end(), leaf), Node[father].end());
+        // Decrease fathers degree by 1 and check if fathers degree == 1
+        if (--degree[father] == 1) singleEdgedNodes.push(father);
+        // Add num of leaf's chindrens to father + 1
+        ++numOfChildren[father] += numOfChildren[leaf];
+        numVisited++;
+    }
+    return 1;
+}
+
+bool randomWalk (){
+    // Cycle at least size 3 and last father should not empty
+    if ((numVisited > N-3) || Node[father].empty()) return 0;
+    unsigned int randomUnvisitedNode = father;
+    // Random Walk
+    do{
+        // if degree > 2 not simple cycle
+        if (degree[randomUnvisitedNode] > 2) return 0;
+        numVisited++;
+        
+        // Add to final Result
+        finalRoots.push_back(numOfChildren[randomUnvisitedNode]+1);
+        numFinalRoots++;
+        // Go to next random node
+        unsigned int tempFather = Node[randomUnvisitedNode][0];
+        Node[tempFather].erase(std::remove(Node[tempFather].begin(), Node[tempFather].end(), randomUnvisitedNode), Node[tempFather].end());
+        randomUnvisitedNode = tempFather;
+    }while(randomUnvisitedNode != father);
+    //std::cout << "Visited This Area numVisited -> " << numVisited << " N -> " << N <<std::endl;
+    if (numVisited != N) return 0;
+    return 1;
+}
+// Print Results
+void printFinal(){
+    if (recursiveFoo() && randomWalk()){
+        std::sort(finalRoots.begin(),finalRoots.end());
+        printf("CORONA %u\n", numFinalRoots);
+        for (unsigned int i = 0; i < numFinalRoots - 1; i++) {
+            printf("%u ",finalRoots[i]);
+        }
+        printf("%u\n",finalRoots[numFinalRoots - 1] );
+    }
+    else {
+        printf ("NO CORONA\n");
+    }
+}
+
+int main(int argc, char** argv) { 
+    // Faster I/O
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    
+    // Input from file
+    FILE * pFile;
+    pFile = fopen (argv[1], "r");
+    unsigned int T = readLong(pFile);
+    
+    // Input
+    for (unsigned int i = 0; i < T; i++) {
+        N = readLong(pFile);
+        unsigned int M = readLong(pFile);
+        if (M != N) {
+            printf ("NO CORONA\n");
+            continue; 
+        }
+    // Initialize Graph
+        
+        // Initialize arrays
+        father = 0; 
+        numVisited = 0;
+        numFinalRoots = 0;
+        memset(degree,0,N*sizeof(unsigned int));
+        memset(numOfChildren,0,N*sizeof(unsigned int));
+        
+        for (unsigned int j = 0; j < M; j++) {
+            unsigned int insertedNode1 = readLong(pFile);
+            degree[insertedNode1-1]++;
+            unsigned int insertedNode2 = readLong(pFile);
+            degree[insertedNode2-1]++;
+            // Adding Edges <-> to graph
+            Node[insertedNode1-1].push_back(insertedNode2-1);
+            Node[insertedNode2-1].push_back(insertedNode1-1); 
+        }
+        // Print Results
+        printFinal();
+        // Clear all vectors
+        for (unsigned int i = 0; i < N; i++) {
+            Node[i].clear();
+        }
+        // Clear queue
+        singleEdgedNodes = std::queue<unsigned int>();
+        // Clear last Results
+        finalRoots.clear();
+    } 
+    return 0; 
+    } 
